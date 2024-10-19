916c90a3a379a66694ec3e2acb4126161ed7d82c
Evelin Garcia <e.garcia.santander@gmail.com>
2024-10-18 21:48:16 -0300

    cambio en view.py de blog

--- ProyectoPrueba/blog/views.py
+++ ProyectoPrueba/blog/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Bienvenidos al blog</h1>')
+    return HttpResponse('<h1>Bienvenidos a mi blog</h1>')
